9dbfedcb0530bb20e99b3e8487ede7f6c0ebc359
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2026-02-11 19:30:49 +0100

    deployment

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,9 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-require("dotenv").config({ path: "./.env" });
+require("dotenv").config({ path: "../.env" });
 
-const {CELO_SEPOLIA_URL, PRIVATE_KEY } = process.env;
+const {CELO_SEPOLIA_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 
 
@@ -11,14 +11,14 @@ const config: HardhatUserConfig = {
   solidity: "0.8.28",
 
   networks: {
-    liskSepolia: {
+    celoSepolia: {
       url: CELO_SEPOLIA_URL,
       accounts: [`0x${PRIVATE_KEY}`],
       // chainId: <LISK_SEPOLIA_CHAIN_ID>,
     },
   },
   etherscan: {
-    // apiKey: `${ETHERSCAN_API_KEY}`,
+    apiKey: `${ETHERSCAN_API_KEY}`,
   },
 };
 
